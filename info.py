ca3eb78108a68921113ba337777128e74553a047
maran117 <153261389+maran117@users.noreply.github.com>
2024-12-05 21:27:31 +0500

    Update info.py

--- info.py
+++ info.py
@@ -124,10 +124,10 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://statistical-ashlan-tamilflix525-52131f8e.koyeb.app/'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://shallow-marie-ann-tamilflix525-b82d00c7.koyeb.app/'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://{}:{}/".format(FQDN, PORT)
+URL = "https://shallow-marie-ann-tamilflix525-b82d00c7.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://shallow-marie-ann-tamilflix525-b82d00c7.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'safaribotts'))
@@ -142,9 +142,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
+    URL = "https://shallow-marie-ann-tamilflix525-b82d00c7.koyeb.app/".format(FQDN)
 else:
-    URL = "https://{}/".format(FQDN)
+    URL = "https://shallow-marie-ann-tamilflix525-b82d00c7.koyeb.app/".format(FQDN)
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
